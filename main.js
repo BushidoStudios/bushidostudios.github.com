2282e0e5bef39a1cf9df4b76ba37e28ce00b1db8
Dennis Bartlett <bartlett.dc.1@gmail.com>
2016-08-21 22:21:12 +0000

    Pushing

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 var app = angular.module('app', ['ngMaterial', 'ngRoute'])
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-    .when('/', {
-      templateUrl: 'templates/home.html',
-      controller: 'homeCtrl',
-      controllerAs: 'home'
-    })
-    .when('/portfolio/:portfolioId/', {
-      templateUrl: 'templates/portfolio.html',
-      controller: 'portfolioCtrl',
-      controllerAs: 'portfolio'
-    })
-    .otherwise({
-      templateUrl: 'templates/404.html'
-    });
-    
-    $locationProvider.html5Mode({
-      enabled: true
-    });
+.config(['$routeProvider', '$locationProvider', '$mdThemingProvider', function($routeProvider, $locationProvider, $mdThemingProvider) {
+  
+  $mdThemingProvider.theme('default')
+  .dark()
+  .primaryPalette('pink')
+  .accentPalette('orange');
+  
+  $mdThemingProvider.theme('bushido')
+  .dark()
+  .primaryPalette('blue')
+  .accentPalette('orange');
+  
+  $mdThemingProvider.alwaysWatchTheme(true);
+  
+  $routeProvider
+  .when('/', {
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl',
+    controllerAs: 'home'
+  })
+  .when('/portfolio/:portfolioId/', {
+    templateUrl: 'templates/portfolio.html',
+    controller: 'portfolioCtrl',
+    controllerAs: 'portfolio'
+  })
+  .otherwise({
+    templateUrl: 'templates/404.html'
+  });
+  
+  $locationProvider.html5Mode({
+    enabled: true
+  });
+}])
+.controller('mainCtrl', ['$routeParams', '$scope', function ($routeParams, $scope) {
+  $scope.theme = 'bushido';
 }])
 .controller('homeCtrl', ['$routeParams', function ($routeParams) {
 }])
